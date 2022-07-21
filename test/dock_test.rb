d3fa2124da306867c5b457f68b9c705f5089ef1a
Nikhil Shahi <shniks@gmail.com>
2022-07-21 11:17:24 -0700

    Created test and methods to compute charges based on maximum rental time. Completed iteration 3.

--- test/dock_test.rb
+++ test/dock_test.rb
@@ -61,4 +61,28 @@ class DockTest < Minitest::Test
     assert_equal expectation_1, dock.charge(kayak_1)
   end
 
+  def test_it_charges_renters_only_for_maximum_rental_time
+    dock = Dock.new("The Rowing Dock", 3)
+    kayak_1 = Boat.new(:kayak, 20)
+    kayak_2 = Boat.new(:kayak, 20)
+    sup_1 = Boat.new(:standup_paddle_board, 15)
+    patrick = Renter.new("Patrick Star", "4242424242424242")
+    eugene = Renter.new("Eugene Crabs", "1313131313131313")
+    dock.rent(kayak_1, patrick)
+    dock.rent(kayak_2, patrick)
+    dock.rent(sup_1, eugene)
+    sup_1.add_hour
+    sup_1.add_hour
+    sup_1.add_hour
+    sup_1.add_hour # Any hours past the max rental time should not count
+    sup_1.add_hour
+
+    expectation_1 = ({
+      :card_number => "1313131313131313",
+      :amount => 45
+      })
+
+    assert_equal expectation_1, dock.charge(sup_1)
+  end
+
 end
